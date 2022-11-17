b7862c7a429b4a68da6a77f37439f50c7af59aaa
xw <xwlufeiwend@163.com>
2022-11-18 00:47:17 +0800

    zancun

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './routers/index'
 
+import sm2 from 'sm-crypto'
+
 const app = createApp(App);
 app.use(router)
 app.mount('#app')
